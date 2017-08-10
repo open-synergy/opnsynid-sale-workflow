8a5956a603428e74bb4aa5d0d017fca1c3458245
mike <viriyananda.michael@gmail.com>
2017-08-10 14:40:27 +0700

    Adding new module sale_order_warning_no_stock

---
+++ sale_order_warning_no_stock/models/sale_order_line.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, api
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.multi
+    def product_id_change_with_wh(
+            self, pricelist, product, qty=0,
+            uom=False, qty_uos=0, uos=False, name='', partner_id=False,
+            lang=False, update_tax=True, date_order=False, packaging=False,
+            fiscal_position=False, flag=False, warehouse_id=False):
+        res = super(SaleOrderLine, self).product_id_change_with_wh(
+            pricelist, product, qty=qty, uom=uom,
+            qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
+            lang=lang, update_tax=update_tax,
+            date_order=date_order, packaging=packaging,
+            fiscal_position=fiscal_position, flag=flag,
+            warehouse_id=warehouse_id)
+        if 'warning' in res:
+            if u'Not enough stock' in res['warning']['message']:
+                res.update({'warning': {}})
+        return res
