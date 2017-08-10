24e4fd00c8343ae8033137f7a6fb34d4d2ceb5c6
mike <viriyananda.michael@gmail.com>
2017-08-10 14:40:27 +0700

    Adding new module sale_order_warning_no_stock

---
+++ sale_order_warning_no_stock/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Order Warning No Stock",
+    "version": "8.0.1.0.0",
+    "category": "Sale",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "sale_stock",
+    ],
+    "data": [
+    ],
+}
