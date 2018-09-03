83046dddca46c585c39fe612ce45d2bc8087004d
Andhitia Rama <andhitia.r@gmail.com>
2018-09-03 10:12:02 +0700

    [8.0.1.0.0] sale_order_primary_creditor

---
+++ sale_order_primary_creditor/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Customer's Primary Creditor Information on Sales Order",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+        "partner_creditor_debitor",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
