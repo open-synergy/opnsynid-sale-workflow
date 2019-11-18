74c6fad0455fa05752c06816eb20002022cecbd2
mikevhe18 <viriyananda.michael@gmail.com>
2019-11-18 11:25:04 +0700

    [8.0.1.4.0][sale_order_product_policy] Issue: #25

--- sale_order_product_policy/__openerp__.py
+++ sale_order_product_policy/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Order Product Policy",
-    "version": "8.0.1.3.0",
+    "version": "8.0.1.4.0",
     "category": "Sale",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
@@ -14,8 +14,6 @@
         "sale_order_type",
     ],
     "data": [
-        "data/ir_actions_server_data.xml",
-        "data/base_action_rule_data.xml",
         "views/sale_order_type_views.xml",
         "views/sale_order_views.xml",
     ],
--- sale_order_product_policy/models/sale_order_type.py
+++ sale_order_product_policy/models/sale_order_type.py
@@ -8,22 +8,6 @@ from openerp import models, fields, api
 class SaleOrderType(models.Model):
     _inherit = "sale.order.type"
 
-    @api.depends(
-        "allowed_product_categ_ids",
-        "allowed_product_ids",
-    )
-    def _compute_all_allowed_product_ids(self):
-        obj_product = self.env["product.product"]
-        for so_type in self:
-            products = so_type.allowed_product_ids
-            category_ids = so_type.allowed_product_categ_ids.ids
-            criteria = [
-                ("categ_id", "in", category_ids),
-                ("sale_ok", "=", True),
-            ]
-            products += obj_product.search(criteria)
-            so_type.all_allowed_product_ids = products
-
     limit_product_selection = fields.Boolean(
         string="Limit Product Selection",
     )
@@ -41,10 +25,4 @@ class SaleOrderType(models.Model):
         relation="rel_so_type_2_product",
         col1="type_id",
         col2="product_id",
-    )
-    all_allowed_product_ids = fields.Many2many(
-        string="Allowed Product",
-        comodel_name="product.product",
-        compute="_compute_all_allowed_product_ids",
-        store=True,
     )
