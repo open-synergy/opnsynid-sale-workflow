aa2aedae2fe8c6b27ea82e778ba8e82c722ab195
mikevhe18 <viriyananda.michael@gmail.com>
2019-11-18 11:25:04 +0700

    [8.0.1.4.0][sale_order_product_policy] Issue: #25

--- sale_order_product_policy/models/sale_order.py
+++ sale_order_product_policy/models/sale_order.py
@@ -8,26 +8,31 @@ from openerp import models, fields, api
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    @api.multi
     @api.depends(
-        "type_id",
-        "type_id.all_allowed_product_ids",
+        "type_id"
     )
-    def _compute_all_allowed_product_ids(self):
+    def _compute_allowed_product(self):
         obj_product = self.env["product.product"]
-        for so in self:
-            if so.type_id.limit_product_selection:
-                so.all_allowed_product_ids = \
-                    so.type_id.all_allowed_product_ids
+        for document in self:
+            if document.type_id.limit_product_selection:
+                document.allowed_product_ids = \
+                    document.type_id.allowed_product_ids.ids
+                document.allowed_product_categ_ids = \
+                    document.type_id.allowed_product_categ_ids.ids
             else:
-                criteria = [
-                    ("sale_ok", "=", True),
-                ]
-                so.all_allowed_product_ids = \
-                    obj_product.search(criteria)
+                document.allowed_product_ids = \
+                    obj_product.search([("sale_ok", "=", True)])
 
-    all_allowed_product_ids = fields.Many2many(
+    allowed_product_ids = fields.Many2many(
         string="Allowed Products",
         comodel_name="product.product",
-        compute="_compute_all_allowed_product_ids",
+        compute="_compute_allowed_product",
+        store=False,
+    )
+    allowed_product_categ_ids = fields.Many2many(
+        string="Allowed Product Categories",
+        comodel_name="product.category",
+        compute="_compute_allowed_product",
         store=False,
     )
