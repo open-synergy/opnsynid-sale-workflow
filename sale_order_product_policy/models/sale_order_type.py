c12c479f1f1e1efcc45d639c46350c37d8619cd1
mikevhe18 <viriyananda.michael@gmail.com>
2019-11-18 11:25:04 +0700

    [8.0.1.4.0][sale_order_product_policy] Issue: #25

--- sale_order_product_policy/models/sale_order_type.py
+++ sale_order_product_policy/models/sale_order_type.py
@@ -2,27 +2,11 @@
 # 2017 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from openerp import models, fields
 
 
 class SaleOrderType(models.Model):
     _inherit = "sale.order.type"
-
-    @api.depends(
-        "allowed_product_categ_ids",
-        "allowed_product_ids",
-    )
-    def _compute_all_allowed_product_ids(self):
-        obj_product = self.env["product.product"]
-        for so_type in self:
-            products = so_type.allowed_product_ids
-            category_ids = so_type.allowed_product_categ_ids.ids
-            criteria = [
-                ("categ_id", "in", category_ids),
-                ("sale_ok", "=", True),
-            ]
-            products += obj_product.search(criteria)
-            so_type.all_allowed_product_ids = products
 
     limit_product_selection = fields.Boolean(
         string="Limit Product Selection",
@@ -41,10 +25,4 @@ class SaleOrderType(models.Model):
         relation="rel_so_type_2_product",
         col1="type_id",
         col2="product_id",
-    )
-    all_allowed_product_ids = fields.Many2many(
-        string="Allowed Product",
-        comodel_name="product.product",
-        compute="_compute_all_allowed_product_ids",
-        store=True,
     )
