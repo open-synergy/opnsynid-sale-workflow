c97550db5606e05fd3403f5bf1ead3bb5c840c2b
Andhitia Rama <andhitia.r@gmail.com>
2020-03-15 20:22:27 +0700

    [8.0.1.0.0] sale_order_type_active

---
+++ sale_order_type_active/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Active Field on Sale Order Type",
+    "version": "8.0.1.0.0",
+    "category": "Sale Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "sale_order_type"
+    ],
+    "data": [
+        "views/sale_order_type_views.xml",
+    ],
+}
