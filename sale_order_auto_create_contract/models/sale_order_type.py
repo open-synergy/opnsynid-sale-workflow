c577d9ca536c14834305da8eec64f958b8893ae6
Andhitia Rama <andhitia.r@gmail.com>
2020-08-20 16:48:41 +0700

    [8.0.1.0.0] sale_order_auto_create_contract

---
+++ sale_order_auto_create_contract/models/sale_order_type.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class SaleOrderType(models.Model):
+    _inherit = "sale.order.type"
+
+    auto_create_contract = fields.Boolean(
+        string="Auto Create Contract",
+        default=False,
+    )
